6e0ee2ffedb7c2495f2d2d1daee83b7e2398bd30
Honishmiran <66351066+Honishmiran@users.noreply.github.com>
2020-08-12 11:33:02 +0530

    Add files via upload

---
+++ honiMVC/src/main/resources/AddContol.java
@@ -0,0 +1,11 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AddContol {
+	
+	@RequestMapping("/add")
+ public void add() {
+	 System.out.println("Im here");
+ }
+}
